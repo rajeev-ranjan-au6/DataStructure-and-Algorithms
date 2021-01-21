02c875180a854754e3ef09899392d7d19c169511
Rajeev Ranjan <50344183+rajeevranjancom@users.noreply.github.com>
2021-01-22 00:33:09 +0530

    bs.cpp

--- bs.cpp
+++ bs.cpp
@@ -1,47 +1,45 @@
-edit
-play_arrow
-
-brightness_4
-// C++ program to implement recursive Binary Search 
+// C++ program for implementation of Bubble sort 
 #include <bits/stdc++.h> 
 using namespace std; 
-  
-// A recursive binary search function. It returns 
-// location of x in given array arr[l..r] is present, 
-// otherwise -1 
-int binarySearch(int arr[], int l, int r, int x) 
+
+void swap(int *xp, int *yp) 
 { 
-    if (r >= l) { 
-        int mid = l + (r - l) / 2; 
-  
-        // If the element is present at the middle 
-        // itself 
-        if (arr[mid] == x) 
-            return mid; 
-  
-        // If element is smaller than mid, then 
-        // it can only be present in left subarray 
-        if (arr[mid] > x) 
-            return binarySearch(arr, l, mid - 1, x); 
-  
-        // Else the element can only be present 
-        // in right subarray 
-        return binarySearch(arr, mid + 1, r, x); 
-    } 
-  
-    // We reach here when element is not 
-    // present in array 
-    return -1; 
+	int temp = *xp; 
+	*xp = *yp; 
+	*yp = temp; 
 } 
-  
-int main(void) 
+
+// A function to implement bubble sort 
+void bubbleSort(int arr[], int n) 
 { 
-    int arr[] = { 2, 3, 4, 10, 40 }; 
-    int x = 10; 
-    int n = sizeof(arr) / sizeof(arr[0]); 
-    int result = binarySearch(arr, 0, n - 1, x); 
-    (result == -1) ? cout << "Element is not present in array"
-                   : cout << "Element is present at index " << result; 
-    return 0; 
+	int i, j; 
+	for (i = 0; i < n-1; i++)	 
+	
+	// Last i elements are already in place 
+	for (j = 0; j < n-i-1; j++) 
+		if (arr[j] > arr[j+1]) 
+			swap(&arr[j], &arr[j+1]); 
 } 
+
+/* Function to print an array */
+void printArray(int arr[], int size) 
+{ 
+	int i; 
+	for (i = 0; i < size; i++) 
+		cout << arr[i] << " "; 
+	cout << endl; 
+} 
+
+// Driver code 
+int main() 
+{ 
+	int arr[] = {64, 34, 25, 12, 22, 11, 90}; 
+	int n = sizeof(arr)/sizeof(arr[0]); 
+	bubbleSort(arr, n); 
+	cout<<"Sorted array: \n"; 
+	printArray(arr, n); 
+	return 0; 
+} 
+
+// This code is contributed by rathbhupendra 
 
